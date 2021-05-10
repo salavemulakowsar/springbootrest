05042acbc4725ec0afd7ec4c84defcd380c3d959
kowsar <salavemulak@gmail.com>
2021-05-10 11:53:18 +0530

    inmemoryauthentication

---
+++ SpringBootRest/src/main/java/com/example/demo/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		//set the config on the auth object
+		auth.inMemoryAuthentication()
+		.withUser("kowsar")
+		.password("salavemula")
+		.roles("USER");
+	}
+
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+
+}
